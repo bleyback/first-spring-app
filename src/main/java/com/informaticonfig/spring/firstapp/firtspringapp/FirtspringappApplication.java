9e2fa83613726a8ac9d6ff6c9d2187aa30f42319
bleyback <samuelius32@gmail.com>
2025-03-18 13:19:16 -0600

    app init

---
+++ src/main/java/com/informaticonfig/spring/firstapp/firtspringapp/FirtspringappApplication.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.firstapp.firtspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirtspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirtspringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informaticonfig/spring/firstapp/firtspringapp/FirtspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.firstapp.firtspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
